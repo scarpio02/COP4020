7d2b169c28ab34a5e53b637ab8c9a9130b7b93d7
scarpio02 <scarpio@ufl.edu>
2023-09-16 15:14:14 -0400

    Initial lexer setup

--- src/edu/ufl/cise/cop4020fa23/Lexer.java
+++ src/edu/ufl/cise/cop4020fa23/Lexer.java
@@ -17,6 +17,14 @@ import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
 public class Lexer implements ILexer {
 
 	String input;
+	private int currLine = 1;
+	private int currColumn = 1;
+	private int pos = 0;
+	private int startPos = 0;
+	private enum State {START, IN_IDENT, HAVE_ZERO, HAVE_DOT, IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS}
+	State state = State.START;
+
+	boolean validToken = true;
 
 
 	public Lexer(String input) {
@@ -25,6 +33,55 @@ public class Lexer implements ILexer {
 
 	@Override
 	public IToken next() throws LexicalException {
+
+		while (validToken) {
+			char ch = input.charAt(pos);
+			switch (state) {
+				case START -> {
+					startPos = pos;  //save position of first char in token
+					switch (ch) {
+						case ' ', '\t', '\n', '\r' -> {pos++;}
+						case '+' -> { //handle all single char tokens like this
+							// create token:kind = Kind.PLUS, position = startPos, length 1;
+							pos++;
+						}
+						case '=' -> {
+							state = State.HAVE_EQ;
+							pos++;
+						}
+						case 0 -> {
+							//this is the end of the input, add an EOF token and return;
+						}
+					}
+
+				}
+				case IN_IDENT-> {
+					throw new UnsupportedOperationException("IN_IDENT not implemented");
+				}
+				case HAVE_ZERO -> {
+					throw new UnsupportedOperationException("HAVE_ZERO not implemented");
+				}
+				case HAVE_DOT -> {
+					throw new UnsupportedOperationException("HAVE_DOT not implemented");
+				}
+				case IN_FLOAT -> {
+					throw new UnsupportedOperationException("IN_FLOAT not implemented");
+				}
+				case IN_NUM -> {
+					throw new UnsupportedOperationException("IN_NUM not implemented");
+				}
+				case HAVE_EQ -> {
+					throw new UnsupportedOperationException("HAVE_EQ not implemented");
+				}
+				case HAVE_MINUS -> {
+					throw new UnsupportedOperationException("HAVE_MINUS not implemented");
+				}
+				default -> {
+					throw new IllegalStateException("lexer bug");
+				}
+			}
+		}
+
 		return new Token(EOF, 0, 0, null, new SourceLocation(1, 1));
 	}
 
