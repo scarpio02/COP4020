2b17a6d044fd4f245d9a7cc239848038892e7628
scarpio02 <scarpio@ufl.edu>
2023-11-05 00:06:41 -0400

    HW3 fixed! (sym table fixed)

--- src/edu/ufl/cise/cop4020fa23/SymbolTable.java
+++ src/edu/ufl/cise/cop4020fa23/SymbolTable.java
@@ -3,6 +3,7 @@ package edu.ufl.cise.cop4020fa23;
 import edu.ufl.cise.cop4020fa23.ast.NameDef;
 import edu.ufl.cise.cop4020fa23.exceptions.TypeCheckException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -10,17 +11,15 @@ public class SymbolTable {
     int current_num;
     int next_num;
     Stack<Integer> scope_stack;
-    HashMap<String, Entry> symbol_table;
+    HashMap<String, ArrayList<Entry>> symbol_table;
 
     class Entry {
         NameDef nameDef;
         int scopeID;
-        Entry link = null;
 
-        Entry(NameDef nd, int id, Entry e) {
+        Entry(NameDef nd, int id) {
             nameDef = nd;
             scopeID = id;
-            link = e;
         }
     }
 
@@ -28,7 +27,7 @@ public class SymbolTable {
          current_num = 0;
          next_num = 1;
          scope_stack = new Stack<Integer>();
-         symbol_table = new HashMap<String, Entry>();
+         symbol_table = new HashMap<String, ArrayList<Entry>>();
     }
     void enterScope() {
         current_num = next_num++;
@@ -42,31 +41,27 @@ public class SymbolTable {
     //FIXME: Infinite looping in symbol table. MUST FIX!!!!!!!!!!
     void insert(NameDef name) throws TypeCheckException {
         if (symbol_table.containsKey(name.getName())) {
-            Entry currE = symbol_table.get(name.getName());
-            while (currE != null) {
-                if (currE.scopeID == current_num) {
+            for (int i = 0; i < symbol_table.get(name.getName()).size(); i++)
+            {
+                if (symbol_table.get(name.getName()).get(i).scopeID == current_num)
+                {
                     throw new TypeCheckException(name.getName() + " is already defined in scope");
                 }
-                currE = currE.link;
             }
-            symbol_table.get(name.getName()).link = symbol_table.get(name.getName());
-            symbol_table.get(name.getName()).nameDef = name;
-            symbol_table.get(name.getName()).scopeID = current_num;
+            symbol_table.get(name.getName()).add(new Entry(name, current_num));
         }
         else {
-
-            symbol_table.put(name.getName(), new Entry(name, current_num, null));
+            symbol_table.put(name.getName(), new ArrayList<Entry>());
+            symbol_table.get(name.getName()).add(new Entry(name, current_num));
         }
     }
     NameDef lookup(String name) throws TypeCheckException {
         if (symbol_table.containsKey(name)) {
-            Entry currE = symbol_table.get(name);
-            while (currE != null) {
-                if (scope_stack.search(currE.scopeID) != -1) {
-                    return currE.nameDef;
-                }
-                else {
-                    currE = currE.link;
+            for (int i = symbol_table.get(name).size() - 1; i >= 0; i--)
+            {
+                if (scope_stack.search(symbol_table.get(name).get(i).scopeID) != -1)
+                {
+                    return symbol_table.get(name).get(i).nameDef;
                 }
             }
         }
